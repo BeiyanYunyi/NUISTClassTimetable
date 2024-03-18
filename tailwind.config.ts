cdb4621110d2a5c6e719c2d9fe651d5920d7263a
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2024-03-18 22:30:47 +0800

    fix: :lipstick: use defaultTheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import typography from '@tailwindcss/typography';
-import type { Config } from 'tailwindcss';
+import { type Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 const config = {
   darkMode: ['class'],
@@ -69,10 +70,7 @@ const config = {
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
       fontFamily: {
-        sans: [
-          'MiSans',
-          'ui-sans-serif,system-ui,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji',
-        ],
+        sans: ['MiSans', ...defaultTheme.fontFamily.sans],
       },
     },
   },
